df8006516344b16852f6cff6509ebec684875686
HuynhDangQuang <113320907+HuynhDangQuang@users.noreply.github.com>
2024-04-04 23:00:09 +0700

    Initial Project

---
+++ Space Shooter/Form1.cs
@@ -0,0 +1,37 @@
+﻿using Space_Shooter.Manager;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Space_Shooter
+{
+    public partial class Form1 : Form
+    {
+        static private System.Windows.Forms.Timer _timer = new System.Windows.Forms.Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            _timer.Interval = 1000;
+            _timer.Tick += new EventHandler(TimerOnTick);
+            _timer.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AudioManager.PlayBGM("bgm1.wav");
+
+        }
+
+        void TimerOnTick(object obj, EventArgs e)
+        {
+            AudioManager.PlaySE("Laser1.wav");
+        }
+    }
+}
---
+++ Space Shooter/Manager/AudioManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Windows.Media.Media3D;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Shooter.Manager
+{
+    internal class AudioManager
+    {
+        // Audio
+        //static private SoundPlayer bgm;
+        [DllImport("winmm.dll")]
+        static private extern Int32 mciSendString(string command, StringBuilder buffer, int bufferSize, IntPtr hwndCallback);
+        static private int _alias = 0;
+
+        static public void PlayBGM(string file)
+        {
+            string link = @"audio\bgm\" + file;
+            string alias = @"sound_bgm";
+            mciSendString(@"close " + alias, null, 0, IntPtr.Zero);
+            mciSendString(@"open " + link + " type waveaudio alias " + alias, null, 0, IntPtr.Zero);
+            mciSendString(@"play " + alias, null, 0, IntPtr.Zero);
+            //mciSendString(@"setaudio " + alias + " volume to 1000", null, 0, IntPtr.Zero);
+        }
+
+        static public void PlaySE(string file)
+        {
+            string link = @"audio\se\" + file;
+            string alias = @"sound" + _alias;
+            mciSendString(@"close " + alias, null, 0, IntPtr.Zero);
+            mciSendString(@"open " + link + " type waveaudio alias " + alias, null, 0, IntPtr.Zero);
+            mciSendString(@"play " + alias, null, 0, IntPtr.Zero);
+            //mciSendString(@"setaudio " + alias + " volume to 1000", null, 0, IntPtr.Zero);
+            if (_alias < 20)
+            {
+                _alias++;
+            }
+            else
+            {
+                _alias = 0;
+            }
+        }
+    }
+}
