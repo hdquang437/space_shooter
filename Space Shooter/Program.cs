3b64f808fc315508c7bd0ce982fd394321b3dff1
leehungw <113320733+leehungw@users.noreply.github.com>
2024-05-06 00:26:48 +0700

    leehungw - init LoginScreen

--- Space Shooter/Program.cs
+++ Space Shooter/Program.cs
@@ -16,7 +16,7 @@ namespace Space_Shooter
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AccountManagement.Login());
         }
     }
 }
